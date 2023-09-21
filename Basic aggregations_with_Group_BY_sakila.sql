a8c7db1e079bd940b5c5fd4e244fd0e5752e28de
Russel Jay Hernandez <93960396+xyseycode@users.noreply.github.com>
2023-09-21 12:20:54 +0800

    Update Basic aggregations_with_Group_BY_sakila.sql

--- Basic aggregations_with_Group_BY_sakila.sql
+++ Basic aggregations_with_Group_BY_sakila.sql
@@ -1,1 +1,93 @@
+USE sakila;
 
+/************************************************************************
+1
+What is the highest amount paid by a customer for a rental?
+In the table payment from the database sakila there is a column called amount that contains the amount paid by a customer for a rental. Please select the correct amount:
+************************************************************************/
+SELECT
+	MAX(amount)
+FROM
+	payment;
+
+
+/************************************************************************
+2
+How many customers are in the customer table?
+Take into consideration that there are no duplicates. Please select the correct option:
+************************************************************************/
+SELECT
+	COUNT(customer_id)
+FROM
+	customer;
+
+/************************************************************************
+When was the first rental done?
+************************************************************************/
+SELECT
+	DISTINCT(rental_date)
+FROM
+	rental
+ORDER BY
+	rental_date ASC
+LIMIT
+	1;
+
+/************************************************************************
+4
+What was the total investment the business made to buy all the films?
+************************************************************************/
+SELECT
+	SUM(replacement_cost)
+FROM
+	film;
+/************************************************************************
+5
+What the highest customer id registered on each store?
+************************************************************************/
+SELECT
+	store_id, MAX(customer_id)
+FROM
+	customer
+GROUP BY
+	store_id;
+
+/************************************************************************
+6
+Retrieve the total amount of payments received by each employee
+************************************************************************/
+SELECT
+	SUM(amount), staff_id
+FROM
+	payment
+GROUP BY
+	staff_id;
+
+/************************************************************************
+7
+Retrieve the number of cities available for each country in the 'city' table
+************************************************************************/
+SELECT
+	count(city_id), country_id
+FROM
+	city
+GROUP BY
+	country_id
+ORDER BY
+	country_id ASC
+LIMIT 15;
+
+/************************************************************************
+8
+When was the last rental of customer 350?
+************************************************************************/
+
+/************************************************************************
+9
+How many actresses are called MARY?
+************************************************************************/
+
+/************************************************************************
+10
+Basic Aggregations(Use WHERE, GROUP BY, ORDER BY)
+************************************************************************/
